2172b35552ebcac9fffce4d3c837512c5831e1da
yoonaiu <free.cs09@nycu.edu.tw>
2023-04-21 17:48:44 +0800

    add select menu for chains

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,25 @@
         </div>
         <div class="d-flex justify-start align-center">
             <v-btn class="mx-2 text-white" @click="toggleTheme()" icon="mdi-theme-light-dark" variant="text"></v-btn>
+            <!-- <v-select hide-details="auto" :items="items" v-model="selectedChain">
+                <template #selection="{item}">
+                    <img :src="getIconURL(item.value.image)" width="30" height="30" />
+                </template>
+                <template #item="{item}">
+                    <div class="d-flex align-center">
+                        <img :src="getIconURL(item.value.image)" width="30" height="30" />
+                        <div>{{ item.value.name }}</div>
+                    </div>
+                </template>
+            </v-select> -->
+
+            <v-select
+                v-model="selected"
+                :items="['ThunderCore', 'ThunderCore Testnet', 'Linea Testnet', 'Gnosis']"
+                hide-details="auto"
+                density="comfortable"
+            ></v-select>
+
             <v-chip v-if="address != ''">{{ beautifyAddress() }}</v-chip>
             <v-btn class="mx-2" v-if="address == ''" @click="connectWallet()" variant="outlined"
                 color="secondary">Connect</v-btn>
@@ -21,6 +40,17 @@
 import { useTheme } from "vuetify"
 import router from '@/router/index'
 import BrowserWallet from "@/composables/wallet"
+import { ref } from "vue"
+
+const selected = ref('ThunderCore')
+
+const items = ref([{name: 'ThunderCore', image: 'ThunderCore_icon'}])
+const selectedChain = ref({name: 'ThunderCore', image: 'ThunderCore_icon'})
+
+const getIconURL = (name: string): string => {
+    const result = new URL(`/src/assets/chains/${name}.svg`, import.meta.url);
+    return result.href;
+};
 
 const theme = useTheme()
 
--- src/composables/wallet.ts
+++ src/composables/wallet.ts
@@ -8,6 +8,15 @@ export default class BrowserWallet {
         const ethereum = (window as any).ethereum
         const provider = new ethers.providers.Web3Provider(ethereum)
 
+        // get the chain selected in the select
+
+        // Check the chain ID of the connected network
+        const chainIdList = [100, 18, 108, 59140]; // Replace with your list of allowed chain IDs
+        const chainId = await provider.getNetwork().then(network => network.chainId)
+        if (!chainIdList.includes(chainId)) {
+            throw new Error("Invalid Ethereum network. Please switch to the correct network.");
+        }
+
         // MetaMask requires requesting permission to connect users accounts
         await provider.send("eth_requestAccounts", [])
 
