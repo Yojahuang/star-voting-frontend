ce5dd402bab7ca6f283859a9b0cb24c465c9c690
You-Jia Huang <yojahuang30613@gmail.com>
2023-06-10 22:54:45 +0800

    feat: part of RWD

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const { smAndDown } = useDisplay()
 
 const cards = reactive([
     {
@@ -50,7 +53,7 @@ const flipcard = (idx: number) => {
 </script>
 
 <template>
-    <div id="my-page" class="mx-auto w-75">
+    <div v-if="!smAndDown" id="my-page" class="mx-auto w-75">
         <div class="text-h4 my-6 font-weight-bold">What we offer</div>
 
         <div class="mx-auto w-100">
@@ -79,6 +82,35 @@ const flipcard = (idx: number) => {
                     </v-expand-transition>
                 </v-col>
             </v-row>
+        </div>
+    </div>
+    <div v-else id="my-page" class="mx-auto w-75">
+        <div class="text-h4 my-6 font-weight-bold">What we offer</div>
+
+        <div class="mx-auto w-100">
+            <div v-for="(card, idx) in cards" :cols="card.flex" class="mx-auto">
+                <v-expand-transition>
+                    <v-card @click="flipcard(idx)" v-show="card.show" class="mx-auto my-4" elevation="21" width="300"
+                        height="300" variant="tonal">
+                        <template v-if="card.showText">
+                            <div class="mx-auto w-75 h-100 d-flex align-center font-center">
+                                {{ card.text }}
+                            </div>
+                        </template>
+                        <template v-else>
+                            <div class="mx-auto w-100 h-100 d-flex align-center font-center">
+                                <div class="w-100 mx-auto">
+                                    <v-icon class="mx-auto w-100" size="40" :icon="card.icon"></v-icon>
+                                    <v-card-title align="center" class="text-center">{{ card.title }}</v-card-title>
+                                    <v-card-subtitle justify="center" class="text-center">
+                                        {{ card.subtitle }}
+                                    </v-card-subtitle>
+                                </div>
+                            </div>
+                        </template>
+                    </v-card>
+                </v-expand-transition>
+            </div>
         </div>
     </div>
 </template>
