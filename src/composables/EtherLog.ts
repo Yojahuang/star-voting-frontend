78babfd0aabf93f444220c302c91a5a01a6b7b7a
yoonaiu <free.cs09@nycu.edu.tw>
2023-06-11 22:35:19 +0800

    update EtherLog but with bug

--- src/composables/EtherLog.ts
+++ src/composables/EtherLog.ts
@@ -2,368 +2,104 @@ import { ethers } from 'ethers'
 import ABI from '@/assets/StarVoting.json'
 import StarVotingContract from '@/composables/StarVoting'
 
-export type chainName =
+export type ChainName =
     | 'Linea Testnet'
     | 'ThunderCore Testnet'
     | 'Chiado(Gnosis) testnet'
 
-const maximumBlockRange = 172800
+type Event =
+    | 'GroupCreated'
+    | 'MemberAdded'
+    | 'MemberUpdated'
+    | 'MemberRemoved'
+    | 'PollCreated'
+    | 'PollStarted'
+    | 'VoteAdded'
+    | 'PollEnded';
 
-// interface ResultMap {
-//     [key: string]: any[]
-// }
-
-// const parseResult: ResultMap = {
-//     'GroupCreated': [],
-//     'MemberAdded': [],
-//     'MemberUpdated': [],
-//     'MemberRemoved': [],
-//     'PollCreated': [],
-//     'PollStarted': [],
-//     'VoteAdded': [],
-//     'PollEnded': []
-// }
-
-// const blockMap = {
-//     'ThunderCore Testnet': {
-//         startBlock: 104926408,
-//     },
-//     'Linea Testnet': {
-//         startBlock: 532009,
-//     },
-//     'Chiado(Gnosis) testnet': {
-//         startBlock: 3584426,
-//     },
-// }
-// type event =
-//     | 'GroupCreated'
-//     | 'MemberAdded'
-//     | 'MemberUpdated'
-//     | 'MemberRemoved'
-//     | 'PollCreated'
-//     | 'PollStarted'
-//     | 'VoteAdded'
-//     | 'PollEnded'
-const eventNameArr = ['GroupCreated', 'MemberAdded', 'MemberUpdated', 'MemberRemoved', 'PollCreated', 'PollStarted', 'VoteAdded', 'PollEnded'] as const;
-type event = 'GroupCreated' | 'MemberAdded' | 'MemberUpdated' | 'MemberRemoved' | 'PollCreated' | 'PollStarted' | 'VoteAdded' | 'PollEnded';
-
-let blockMap = {
-    'ThunderCore Testnet': {
-        'GroupCreated': { startBlock: 104926408, result: [] as any[] },
-        'MemberAdded': { startBlock: 104926408, result: [] as any[] },
-        'MemberUpdated': { startBlock: 104926408, result: [] as any[] },
-        'MemberRemoved': { startBlock: 104926408, result: [] as any[] },
-        'PollCreated': { startBlock: 104926408, result: [] as any[] },
-        'PollStarted': { startBlock: 104926408, result: [] as any[] },
-        'VoteAdded': { startBlock: 104926408, result: [] as any[] },
-        'PollEnded': { startBlock: 104926408, result: [] as any[] },
-    },
-    'Linea Testnet': {
-        'GroupCreated': { startBlock: 532009, result: [] as any[] },
-        'MemberAdded': { startBlock: 532009, result: [] as any[] },
-        'MemberUpdated': { startBlock: 532009, result: [] as any[] },
-        'MemberRemoved': { startBlock: 532009, result: [] as any[] },
-        'PollCreated': { startBlock: 532009, result: [] as any[] },
-        'PollStarted': { startBlock: 532009, result: [] as any[] },
-        'VoteAdded': { startBlock: 532009, result: [] as any[] },
-        'PollEnded': { startBlock: 532009, result: [] as any[] },
-    },
-    'Chiado(Gnosis) testnet': {
-        'GroupCreated': { startBlock: 3584426, result: [] as any[] },
-        'MemberAdded': { startBlock: 3584426, result: [] as any[] },
-        'MemberUpdated': { startBlock: 3584426, result: [] as any[] },
-        'MemberRemoved': { startBlock: 3584426, result: [] as any[] },
-        'PollCreated': { startBlock: 3584426, result: [] as any[] },
-        'PollStarted': { startBlock: 3584426, result: [] as any[] },
-        'VoteAdded': { startBlock: 3584426, result: [] as any[] },
-        'PollEnded': { startBlock: 3584426, result: [] as any[] },
-    },
+const URLMap = {
+    'Linea Testnet': 'https://explorer.goerli.linea.build/api/eth-rpc',
+    'ThunderCore Testnet': 'https://explorer-testnet.thundercore.com/api/eth-rpc',
+    'Chiado(Gnosis) testnet': 'https://blockscout.com/gnosis/chiado/api/eth-rpc',
 }
 
-const getGroupCreatedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.GroupCreated()
+const eventKeyword = [
+    "pollId", "coordinator", "encryptionKey", "vote", "decryptionKey",
+    "groupId", "merkleTreeDepth", "zeroValue", "index", "identityCommitment",
+    "merkleTreeRoot", "newIdentityCommitment"
+]
 
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
+const eventSignatureMap = {
+    'GroupCreated': '0x0d000126c26c1bbe400fd2332187f75d58b69306f9ec47b408686189d3a00833',
+    'MemberAdded': '0x19239b3f93cd10558aaf11423af70c77763bf54f52bcc75bfa74d4d13548cde9',
+    'MemberUpdated': '0xea3588e4a2a0c93d6a0e69dfeaf7496f43ccccf02ad9ce0a5b7627cbca4b61b1',
+    'MemberRemoved': '0x3108849c053c77b8073a11256dffb5ffd5b55e93e105a355e1c9061db890d871',
 
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-        const parsedEvent = {
-            groupId: args.groupId.toString() as string,
-            merkleTreeDepth: args.merkleTreeDepth.toString() as string,
-            zeroValue: args.zeroValue.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
+    'PollCreated': '0xb447d309fd3b1a27466cdb08ccc02ee5188e9c71fc58af042bafe3e0bb451230',
+    'PollStarted': '0xd98b04626cc7c61b3e1903c1fdf653b72f88d67afa9b71ccb81c79cb967cfdce',
+    'VoteAdded': '0x0ac6ee434e2e22214897d8dcdfce63aaae60241483ea57f3b72b404221b2ceac',
+    'PollEnded': '0x6cccde3929a60a7b5f3ef66f6d0b185ac2e1962c4b42c9acc4c4bed6a664e3a5'
 }
 
-const getMemberAddedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.MemberAdded()
 
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-        const parsedEvent = {
-            groupId: args.groupId.toString() as string,
-            index: args.index.toString() as string,
-            identityCommitment: args.identityCommitment.toString() as string,
-            merkleTreeRoot: args.merkleTreeRoot.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getMemberUpdatedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.MemberUpdated()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-        const parsedEvent = {
-            groupId: args.groupId.toString() as string,
-            index: args.index.toString() as string,
-            identityCommitment: args.identityCommitment.toString() as string,
-            newIdentityCommitment:
-                args.newIdentityCommitment.toString() as string,
-            merkleTreeRoot: args.merkleTreeRoot.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getMemberRemovedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.MemberRemoved()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-        const parsedEvent = {
-            groupId: args.groupId.toString() as string,
-            index: args.index.toString() as string,
-            identityCommitment: args.identityCommitment.toString() as string,
-            merkleTreeRoot: args.merkleTreeRoot.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getPollCreatedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.PollCreated()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-        const parsedEvent = {
-            pollId: args.pollId.toString() as string,
-            coordinator: args.coordinator.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getPollStartedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.PollStarted()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-
-        const parsedEvent = {
-            pollId: args.pollId.toString() as string,
-            coordinator: args.coordinator.toString() as string,
-            encryptionKey: args.encryptionKey.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getVoteAddedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.VoteAdded()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-
-        const parsedEvent = {
-            pollId: args.pollId.toString() as string,
-            vote: args.vote.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-const getPollEndedEvent = async (
-    contract: ethers.Contract,
-    startBlock: number,
-    endBlock: number
-) => {
-    const filter = contract.filters.PollEnded()
-
-    let events = await contract.queryFilter(filter, startBlock, endBlock)
-
-    const parsedEvents = []
-    for (let i = 0; i < events.length; i++) {
-        const args: any = events[i].args
-
-        const parsedEvent = {
-            pollId: args.pollId.toString() as string,
-            coordinator: args.coordinator.toString() as string,
-            decryptionKey: args.decryptionKey.toString() as string,
-        }
-        parsedEvents.push(parsedEvent)
-    }
-    return parsedEvents
-}
-
-// 1. wait for star's address to fill in
-// 2. find provider url -> not really sure
-function getContract(chainName: chainName) {
-    let provider: any
-    let contract
-
-    if (chainName === 'ThunderCore Testnet') {
-        // chain 1 deployed -> on going -> good
-        provider = new ethers.providers.WebSocketProvider(
-            'wss://testnet-ws.thundercore.com'
-        )
-    } else if (chainName === 'Linea Testnet') {
-        provider = new ethers.providers.JsonRpcProvider(
-            'https://rpc.goerli.linea.build/'
-        )
-    } else if (chainName === 'Chiado(Gnosis) testnet') {
-        // chain 2 deployed -> on going
-        provider = new ethers.providers.JsonRpcProvider(
-            'https://rpc.chiadochain.net'
-        )
-    }
-
+function getRequestBody(eventName: Event) {
     const starVotingContract = new StarVotingContract()
     starVotingContract.init()
-    const address = starVotingContract.address
 
-    contract = new ethers.Contract(address, ABI, provider)
-    return { contract, provider }
-}
-
-// Get the block number
-const getBlockNumber = async (provider: any) => {
-    const blockNumber = await provider.getBlockNumber()
-    return blockNumber
-}
-
-const getInfo = async (eventName: event, contract: ethers.Contract, startBlock: number, endBlock: any) => {
-    // for group
-    if (eventName === 'GroupCreated') {
-        let info
-        info = await getGroupCreatedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'MemberAdded') {
-        let info
-        info = await getMemberAddedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'MemberUpdated') {
-        let info
-        info = await getMemberUpdatedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'MemberRemoved') {
-        let info
-        info = await getMemberRemovedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'PollCreated') {
-        let info
-        info = await getPollCreatedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'PollStarted') {
-        let info
-        info = await getPollStartedEvent(contract, startBlock, endBlock)
-        return info
-    } else if (eventName === 'VoteAdded') {
-        let info = await getVoteAddedEvent(contract, startBlock, endBlock)
-        return info
-    } else {
-        let info
-        info = await getPollEndedEvent(contract, startBlock, endBlock)
-        return info
-    }
-}
-
-const getEvents = async (chainName: chainName, eventName: event) => {
-    const { contract, provider } = getContract(chainName)
-
-    const endBlock = await getBlockNumber(provider)
-    let currentStartBlock = blockMap[chainName][eventName].startBlock
-
-    while (currentStartBlock < endBlock) {
-        let currentEndBlock = currentStartBlock + maximumBlockRange - 1
-        if (currentEndBlock > endBlock) currentEndBlock = endBlock
-
-        const tmpResult = await getInfo(eventName, contract, currentStartBlock, currentEndBlock)
-        blockMap[chainName][eventName].result.push(...tmpResult)
-
-        currentStartBlock = currentEndBlock + 1
+    // TODO: use pagniation when event log have more than 1000+ data
+    const requestBody = {
+        "id": 0,
+        "jsonrpc": "2.0",
+        "method": "eth_getLogs",
+        "params": [
+            {
+                "address": starVotingContract.address,
+                "fromBlock": "earliest",
+                "toBlock": "latest",
+                "topics": [
+                    eventSignatureMap[eventName]
+                ]
+            }
+        ]
     }
 
-    // renew startBlock
-    blockMap[chainName][eventName].startBlock = endBlock + 1
-
-    return blockMap[chainName][eventName].result
+    return requestBody
 }
 
-export {
-    getEvents,
-    getVoteAddedEvent,
-    getPollStartedEvent,
-    getPollCreatedEvent,
-    getMemberRemovedEvent,
-    getMemberUpdatedEvent,
-    getMemberAddedEvent,
-    getGroupCreatedEvent,
-    getPollEndedEvent,
+async function sendRequest(chainName: ChainName, body: any) {
+    const url = URLMap[chainName]
+    const data =  await fetch(url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    return data.json()
 }
+
+
+function parseLog(data: string, topics: string[]) {
+    const iface = new ethers.utils.Interface(ABI)
+    return iface.parseLog({data, topics})
+}
+
+const getEvents = async (chainName: ChainName, eventName: Event) => {
+    const requestBody = getRequestBody(eventName)
+    const data = await sendRequest(chainName, requestBody)
+    const result: any[] = []
+
+    data.result.forEach((log: any) => {
+        const args = parseLog(log.data, log.topics).args
+        const args2: any = {}
+        eventKeyword.forEach((keyword) => {
+            if (args[keyword])
+                args2[keyword] = args[keyword].toString()
+        })
+        result.push(args2)
+    })
+
+    return result
+}
+
+export { getEvents }
